60ea37ec0fcd2e4067d0f0d3966e017cf6176c2d
brigita <brigita.cironytee@gmail.com>
2018-02-13 12:30:11 +0200

    Komito zinute

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pirma_uzduotis
+//
+//  Created by Brigita Cironyte on 13/02/2018.
+//  Copyright © 2018 Brigita Cironyte. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
