d31d72e80bed9365fe15a9c4b3f25d9a3cc03fc6
brigita <brigita.cironytee@gmail.com>
2018-02-13 13:57:28 +0200

    Pradeta daryti pirma uzduotis

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
-//
-//  main.cpp
-//  pirma_uzduotis
-//
-//  Created by Brigita Cironyte on 13/02/2018.
-//  Copyright © 2018 Brigita Cironyte. All rights reserved.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <string>
+using std::cout; using std::cin; using std::endl; using std::string;
+int main()
+{
+    cout << "Įveskite savo vardą: ";
+    string vardas;
+    cin >> vardas;
+    cout << "Įveskite savo pavardę: ";
+    string pavarde;
+    cin >> pavarde;
+    cout << "Įveskite, kiek namų darbų turėjo "<<vardas<<" "<<pavarde<<" : ";
+    auto nd_sk=0;
+    cin >> nd_sk;
+    for (auto i=1;i<nd_sk;i++)
+    {
+    cout << "Įveskite, kiek " <<vardas<<" "<<pavarde<< " gavo is "<<i<<" namų darbų: ";
+    }
+    cout << "Įveskite, kiek "<<vardas<<" "<<pavarde<<" gavo iš egzamino: ";
+    auto egz_paz=0;
+    cin >> egz_paz;
     return 0;
 }
+
